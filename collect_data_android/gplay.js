f2acd342becd64f33dba58ce29d6e4aa98b5d5fd
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-06 13:52:06 -0500

    pull top 120 free and grossing games

--- collect_data_android/gplay.js
+++ collect_data_android/gplay.js
@@ -1,3 +1,15 @@
 var gplay = require('google-play-scraper');
-gplay.app({appId: 'com.dxco.pandavszombies'})
+
+gplay.list({
+  category: gplay.category.GAME,
+  collection: gplay.collection.TOP_FREE,
+  num: 120
+  })
   .then(console.log, console.log);
+
+gplay.list({
+  category: gplay.category.GAME,
+  collection: gplay.collection.GROSSING,
+  num: 120
+  })
+  .then(console.log, console.log)
