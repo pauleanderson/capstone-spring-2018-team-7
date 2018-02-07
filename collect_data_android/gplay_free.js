2afb3ef64925a275a3bd7634daf2c8660f6df82a
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-07 14:42:22 -0500

    function with command line args country code and starting index

---
+++ collect_data_android/gplay_free.js
@@ -0,0 +1,30 @@
+// npm install google-play-scraper
+// npm install command-line-args
+
+var gplay = require('google-play-scraper');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+  {name: 'country_code', alias: 'c', type: String},
+  {name: 'start_index', alias: 'i', type: Number}
+]
+
+const options = commandLineArgs(optionDefinitions)
+// console.log(options)
+// console.log(options.country_code)
+// console.log(options.collection_code)
+// console.log(options.start_index)
+
+
+var top = function(var_country_code, var_start_index) {
+  gplay.list({
+    category: gplay.category.GAME,
+    collection: gplay.collection.TOP_FREE,
+    num : 100,
+    country: var_country_code,
+    start : var_start_index
+  })
+  .then(console.log, console.log);
+} 
+
+top(options.country_code, options.start_index);
