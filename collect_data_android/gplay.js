1cb91cca85523122ad8cadcc9054656809e10eb1
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-07 10:36:57 -0500

    working function

--- collect_data_android/gplay.js
+++ collect_data_android/gplay.js
@@ -1,113 +1,17 @@
 // npm install google-play-scraper
 
-'use strict';
+//'use strict';
 var gplay = require('google-play-scraper');
-const fs = require('fs');
 
-var err_handler = function(err) {
-  console.log(err);
+var top = function(country_code, collection_code, start_index) {
+  gplay.list({
+    category: gplay.category.GAME,
+    collection: collection_code,
+    num : 100,
+    country: country_code,
+    start : start_index
+  })
+  .then(console.log, console.log);
 }
 
-au_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'au'
-  });
- // .then(JSON.stringify, err_handler)
- // .then(fs.writeFileSync('au_free.json'), err_handler);
-data_au_free = JSON.stringify(au_free);
-fs.writeFileSync('au_free.json', data_au_free);
-
-
-au_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'au'
-  });
-
-nz_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'nz'
-  });
-
-nz_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'nz'
-  });
-
-se_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'se'
-  }); 
-
-se_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'se'
-  });
-
-dk_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'dk'
-  }); 
-
-dk_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'dk'
-  }); 
-
-no_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'no'
-  }); 
-
-no_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'no'
-  });  
-
-at_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'at'
-  }); 
-
-at_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'at'
-  });  
-
-ph_free = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.TOP_FREE,
-  num: 120,
-  country: 'ph'
-  }); 
-
-ph_gross = gplay.list({
-  category: gplay.category.GAME,
-  collection: gplay.collection.GROSSING,
-  num: 120,
-  country: 'ph'
-  });
-
+top('au', gplay.collection.TOP_FREE, 0);
