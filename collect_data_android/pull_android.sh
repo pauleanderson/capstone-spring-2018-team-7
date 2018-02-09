cc82cb436aebb60fdbdb4ff1446fc64ac30a2a9c
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-09 10:59:49 -0500

    Add JSON files

--- collect_data_android/pull_android.sh
+++ collect_data_android/pull_android.sh
@@ -10,6 +10,8 @@ node gplay_free.js -c 'au' -i 100 > ~/capstone-spring-2018-team-7/collect_data_a
 node gplay_free.js -c 'au' -i 200 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_200.json
 node gplay_free.js -c 'au' -i 300 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_300.json
 node gplay_free.js -c 'au' -i 400 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_400.json
+cat ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_400.json > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free.json
+rm ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_free_400.json
 
 #au gross
 node gplay_gross.js -c 'au' -i 0 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_0.json
@@ -17,6 +19,8 @@ node gplay_gross.js -c 'au' -i 100 > ~/capstone-spring-2018-team-7/collect_data_
 node gplay_gross.js -c 'au' -i 200 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_200.json
 node gplay_gross.js -c 'au' -i 300 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_300.json
 node gplay_gross.js -c 'au' -i 400 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_400.json
+cat ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_400.json > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross.json
+rm ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_gross_400.json
 
 #au trend
 node gplay_trend.js -c 'au' -i 0 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_0.json
@@ -24,4 +28,5 @@ node gplay_trend.js -c 'au' -i 100 > ~/capstone-spring-2018-team-7/collect_data_
 node gplay_trend.js -c 'au' -i 200 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_200.json
 node gplay_trend.js -c 'au' -i 300 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_300.json
 node gplay_trend.js -c 'au' -i 400 > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_400.json
-
+cat ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_400.json > ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend.json
+rm ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_0.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_100.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_200.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_300.json ~/capstone-spring-2018-team-7/collect_data_android/JSON/au_trend_400.json
