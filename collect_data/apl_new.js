7c838c1d30211a1dd872dc35044865d399937815
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-17 21:33:27 -0500

    Add node scripts to pull from App Store

---
+++ collect_data/apl_new.js
@@ -0,0 +1,22 @@
+// npm install app-store-scraper
+// npm install command-line-args
+
+var store = require('app-store-scraper');
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+  {name: 'country_code', alias: 'c', type: String}
+]
+
+const options = commandLineArgs(optionDefinitions)
+
+var top = function(var_country_code) {
+  store.list({
+    collection: store.collection.NEW_GAMES_IOS,
+    num : 200,
+    country: var_country_code,
+  })
+  .then(console.log, console.log);
+} 
+
+top(options.country_code);
