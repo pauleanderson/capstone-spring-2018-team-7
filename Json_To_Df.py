57bd41478d6f78ed89bd4655ded68b18cf4e879e
Kiddzt <kiddzt95@gmail.com>
2018-03-05 13:59:40 -0500

    Updating JSon to DF

--- Json_To_Df.py
+++ Json_To_Df.py
@@ -5,19 +5,14 @@ import re
 import demjson
 from bson import json_util
 import datetime
-
+import pandas as pd
 #jsonData = pd.read_json(fileName, orient="records")
 
 connection = pymongo.MongoClient("mongodb://localhost")
 db = connection.apps
-
 apple = db.apple
-result = apple.delete_many({})
 
-dir='/Users/ZacharyKidd/capstone-spring-2018-team-7/collect_data/apple'
-
-data = open('at_gross_2018-02-21.json').read()
-data = data.replace(": undefined" , ": \"undefined\"")
+#data = data.replace(": undefined" , ": \"undefined\"")
 
 cursor = apple.find({"country":"au", "chart":"free", "date":"2018-03-05"},
                    {"_id":False, "title":True, "rank":True})
