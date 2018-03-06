11f5681f7912852befd69400b14e736a103c0975
koatliky <koatliky@gmail.com>
2018-03-05 23:46:03 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -15,5 +15,6 @@ new_df = pd.concat(g for _, g in df.groupby("appId") if len(g) > 1)
 
 pivot = df.pivot_table(index = "appId", columns = "date", values = "rank")
 print(pivot.to_string().translate(non_pbm_map))
+print(pivot.length())
 
 
