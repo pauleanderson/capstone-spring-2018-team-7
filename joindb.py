d6ca670415aa5215cb9cf5c7930b6c1f9c4931df
koatliky <koatliky@gmail.com>
2018-03-05 23:00:21 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -14,7 +14,7 @@ non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 #Find all the appId duplicates copy into a new dataframe and sort
 new_df = pd.concat(g for _, g in df.groupby("appId") if len(g) > 1)
-print(new_df.to_string().translate(non_pbm_map))
+#print(new_df.to_string().translate(non_pbm_map))
 
 #Inner join
 df_sliced = new_df[["appId", "country", "rank", "date"]].copy()
