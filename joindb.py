547676c61a7bd825e97344c78fc23c9d2d0c01c2
koatliky <koatliky@gmail.com>
2018-03-05 21:48:22 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -11,7 +11,7 @@ df = pd.DataFrame(list(android.find()))
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 #print(df.loc[1:6, :])
 
-ids = df["_id"]
+ids = df["appId"]
 new_df = df[ids.isin(ids[ids.duplicated()])]
 print(new_df.to_string().translate(non_pbm_map))
 
