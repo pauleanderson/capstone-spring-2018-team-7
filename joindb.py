8b2a5479744aedb225b77dcc6135483a1b84349c
koatliky <koatliky@gmail.com>
2018-03-05 20:43:56 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -6,7 +6,8 @@ import sys
 client = MongoClient ('localhost', 27017)
 data = client.apps
 
-apple = data.apple
-df = pd.DataFrame(list(apple.find()))
+android = data.android
+df = pd.DataFrame(list(android.find()))
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 print(df.to_string().translate(non_pbm_map))
+
