829489047b4c8fe5dd2d9295f328397c14838136
koatliky <koatliky@gmail.com>
2018-03-05 23:14:58 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -17,7 +17,7 @@ new_df = pd.concat(g for _, g in df.groupby("appId") if len(g) > 1)
 #print(new_df.to_string().translate(non_pbm_map))
 
 #Inner join
-left_df = new_df.drop_duplicates(subset="appId")
+left_df = new_df.drop_duplicates(subset="appId", take_last=True)
 df_sliced = new_df[["appId", "country", "rank", "date"]].copy()
 
 
