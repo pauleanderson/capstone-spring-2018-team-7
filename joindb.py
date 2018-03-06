f54ce328507f99703e8e34a5893980b008cbb88d
koatliky <koatliky@gmail.com>
2018-03-05 23:56:33 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -6,7 +6,7 @@ import sys
 client = MongoClient ('localhost', 27017)
 data = client.apps
 
-## start with Android data
+## start with opening data
 
 def set_android ():
   return data.android
@@ -17,8 +17,8 @@ def set_apple ():
 def pick_country(platform, country):
   return pd.DataFrame(list(platform.find({"country":country})))
 
-android = set_android()
-df = pick_country(android, "au")
+
+df = pick_country(set_apple(), "au")
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 pivot = df.pivot_table(index = "appId", columns = "date", values = "rank")
