bb630d04d9c3321c7d29d528d77b80ceb639cbc6
koatliky <koatliky@gmail.com>
2018-03-05 21:49:59 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -7,7 +7,7 @@ client = MongoClient ('localhost', 27017)
 data = client.apps
 
 android = data.android
-df = pd.DataFrame(list(android.find()))
+df = pd.DataFrame(list(android.find({"country":"au"})))
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 #print(df.loc[1:6, :])
 
