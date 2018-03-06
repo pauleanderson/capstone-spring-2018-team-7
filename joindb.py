989c9b035cc262e5d7153c873c7fea9744c2a499
koatliky <koatliky@gmail.com>
2018-03-05 23:55:17 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -11,8 +11,14 @@ data = client.apps
 def set_android ():
   return data.android
 
+def set_apple ():
+  return data.apple
+
+def pick_country(platform, country):
+  return pd.DataFrame(list(platform.find({"country":country})))
+
 android = set_android()
-df = pd.DataFrame(list(android.find({"country":"au"})))
+df = pick_country(android, "au")
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 pivot = df.pivot_table(index = "appId", columns = "date", values = "rank")
