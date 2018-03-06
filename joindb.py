fb6169be03491a794d971b039a63eb968ff2b998
koatliky <koatliky@gmail.com>
2018-03-05 21:37:06 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -9,5 +9,10 @@ data = client.apps
 android = data.android
 df = pd.DataFrame(list(android.find({"country":"au"})))
 ##non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-print(df.loc[1:6, :])
+#print(df.loc[1:6, :])
+
+scores = df["ID"]
+new_df = df[scores.isin(scores[scores.duplicated()])]
+
+print(new_df.to_string().translate(non_pbm_map))
 
