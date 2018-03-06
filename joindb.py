912b39eedc5163135f3230bd97265e9233613c8e
koatliky <koatliky@gmail.com>
2018-03-05 23:45:31 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -10,22 +10,10 @@ data = client.apps
 android = data.android
 df = pd.DataFrame(list(android.find({"country":"au"})))
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
-#print(df.loc[1:6, :])
 
-#Find all the appId duplicates copy into a new dataframe and sort
 new_df = pd.concat(g for _, g in df.groupby("appId") if len(g) > 1)
-#print(new_df.to_string().translate(non_pbm_map))
 
-#Inner join
-#df_sliced = new_df[["appId", "country", "rank", "date"]].copy()
-#left_df = new_df.drop_duplicates(subset="appId", keep = "first")
-
-
-
-#merged_df = left_df.merge(df_sliced, on="appId", validate="one_to_many")
-#print(merged_df.to_string().translate(non_pbm_map))
-
-pivot = new_df.pivot_table(index = "appId", columns = "date", values = "rank")
+pivot = df.pivot_table(index = "appId", columns = "date", values = "rank")
 print(pivot.to_string().translate(non_pbm_map))
 
 
