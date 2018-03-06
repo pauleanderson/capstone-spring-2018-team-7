8275db92576989c66979cc1ac45948ff64079354
koatliky <koatliky@gmail.com>
2018-03-05 22:55:21 -0500

    Update joindb.py

--- joindb.py
+++ joindb.py
@@ -2,10 +2,11 @@ from pymongo import MongoClient
 import pandas as pd
 import sys
 
-## working code
+## connect to the database
 client = MongoClient ('localhost', 27017)
 data = client.apps
 
+## start with Android data
 android = data.android
 df = pd.DataFrame(list(android.find({"country":"au"})))
 non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
@@ -13,7 +14,7 @@ non_pbm_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 #Find all the appId duplicates copy into a new dataframe and sort
 new_df = pd.concat(g for _, g in df.groupby("appId") if len(g) > 1)
-#print(new_df.to_string().translate(non_pbm_map))
+print(new_df.to_string().translate(non_pbm_map))
 
 #Inner join
 df_sliced = new_df[["appId", "country", "rank", "date"]].copy()
