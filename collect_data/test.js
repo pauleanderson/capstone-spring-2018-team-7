8b7277edf31b5e5ed3551426c9b3042b0ed47538
katiebalcewicz <kh.balcewicz@gmail.com>
2018-03-09 10:08:45 -0500

    some files not downloading - running some tests

---
+++ collect_data/test.js
@@ -0,0 +1,64 @@
+var gplay = require('google-play-scraper');
+
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'au',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'nz',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'se',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'dk',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'no',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'at',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
+  
+gplay.list({
+    category: gplay.category.GAME_ACTION,
+    collection: gplay.collection.TOP_FREE,
+    country: 'ph',
+    num: 2,
+    fullDetail : true
+  })
+  .then(console.log, console.log);
