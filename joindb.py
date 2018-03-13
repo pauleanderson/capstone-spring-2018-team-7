2bb74cec8c1075e90324d301f5cb79c1b3b62875
Kiddzt <kiddzt95@gmail.com>
2018-03-13 17:36:26 -0400

    updating joindb.py

--- joindb.py
+++ joindb.py
@@ -64,7 +64,7 @@ print(pivot.loc[pivot["delta rank1"].idxmax()])
 #grabing the games from the data and Aggregating it into a single table.
 
 dfAll = pull_all_countries(set_apple())
-pivotAll = dfAll.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
+pivotAll = dfAll.pivot_table(index = ["title"], columns = "date", values = "rank")
 pivotAll = pivot.fillna(100)
 pivotAll = pivotAll[["delta rank1"]]
 print(pivotAll)
