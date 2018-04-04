12d7dfc589effbb1a6b280f6425a6dbde0d4295f
koatliky <koatliky@gmail.com>
2018-04-04 14:50:48 -0400

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -1,1 +1,2 @@
-$ curl -X POST -H 'Content-type: application/json' --data '{"text":"Allow me to reintrode myself!"}' https://hooks.slack.com/services/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
+$ curl -X POST -H "Content-Type: application/json" \  --data @final.json https://hooks.slack.com/services/T91
+7Z9TPW/BA22BTNBY/FhWKmmFAYDGi0UcfhtSMFZf8
