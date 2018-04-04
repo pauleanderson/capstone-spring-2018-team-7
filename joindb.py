c58293be8b8d17366c4f8dd3d7add5699bf0b5db
Kiddzt <kiddzt95@gmail.com>
2018-04-04 16:18:59 -0400

    added loops to create pivot tables in joindb.py

--- joindb.py
+++ joindb.py
@@ -78,11 +78,19 @@ df_apple_collection = {}
 for country_chart in apple_country_charts:
   df_apple_collection[country_chart] = pick_country(set_apple(), country_chart)
 
+
+df_apple_pivots = {}
+
 for key in df_apple_collection.keys():
-    print("\n" +"="*40)
-    print(key)
-    print("-"*40)
-    print(df_apple_collection[key])
+  df_apple_pivots[key] = df_apple_collection[key].pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
+  df_apple_pivots[key] = df_apple_pivots[key].fillna(101)
+  df_apple_pivots[key] = df_apple_pivots[key].iloc[:,df_apple_pivots[key].shape[1]-6:]
+  df_apple_pivots[key]= last_five_days(df_apple_pivots[key])
+  df_apple_pivots[key].reset_index(inplace = True)
+
+print(df_apple_pivots["au free"])
+
+
 
 '''
 Android country chart
@@ -92,72 +100,18 @@ for country_chart in android_country_charts:
 #country initialzation
 
 
-#Build the pivot table for au
-pivot_au = df_au.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_au = pivot_au.fillna(101)
-pivot_au = pivot_au.iloc[:,pivot_au.shape[1]-6:]
-#print(pivot_au.to_string().translate(unicode()))
-pivot_au = last_five_days(pivot_au)
-pivot_au.reset_index(inplace = True)
-print(pivot_au.to_string().translate(unicode()))
+
 
 '''
-print("")
-print("")
-print("")
-print("The 3 bigest positive change over last 4 days in australia:")
 print(pivot_au.nlargest(3,"delta rank0"))
 print("")
 '''
 
-#nz 
-pivot_nz = df_nz.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_nz = pivot_nz.fillna(101)
-pivot_nz = pivot_nz.iloc[:,pivot_nz.shape[1]-6:]
-pivot_nz = last_five_days(pivot_nz)
-pivot_nz.reset_index(inplace = True)
-#print(pivot_nz.to_string().translate(unicode()))
 
-
-#se
-pivot_se = df_se.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_se = pivot_se.fillna(101)
-pivot_se = pivot_se.iloc[:,pivot_se.shape[1]-6:]
-pivot_se = last_five_days(pivot_se)
-#print(pivot_se.to_string().translate(unicode()))
-
-#dk 
-pivot_dk = df_dk.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_dk = pivot_dk.fillna(101)
-pivot_dk = pivot_dk.iloc[:,pivot_dk.shape[1]-6:]
-pivot_dk = last_five_days(pivot_dk)
-#print(pivot_dk.to_string().translate(unicode()))
-
-#no 
-pivot_no = df_no.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_no = pivot_no.fillna(101)
-pivot_no = pivot_no.iloc[:,pivot_no.shape[1]-6:]
-pivot_no = last_five_days(pivot_no)
-#print(pivot_no.to_string().translate(unicode()))
-
-#at 
-pivot_at = df_at.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_at = pivot_at.fillna(101)
-pivot_at = pivot_at.iloc[:,pivot_at.shape[1]-6:]
-pivot_at = last_five_days(pivot_at)
-#print(pivot_at.to_string().translate(unicode()))
-
-#ph
-pivot_ph = df_ph.pivot_table(index = ["title", "chart"], columns = "date", values = "rank")
-pivot_ph = pivot_ph.fillna(101)
-pivot_ph = pivot_ph.iloc[:,pivot_ph.shape[1]-6:]
-pivot_ph = last_five_days(pivot_ph)
-#print(pivot.to_string().translate(unicode()))
-#print(pivot_ph.to_string().translate(unicode()))
 
 #Need to find a way to grab genre[1] and make sure its gaming.
 #that code goes here
-
+'''
 #mergin data frames into one training set
 
 #print(pivot_se.shape[0])
@@ -165,7 +119,7 @@ pivot_ph = last_five_days(pivot_ph)
 pivot_train = pd.merge(pivot_au,pivot_nz,on = ["title"],how  = "outer")
 print(pivot_train)
 #model
-
+'''
 outliers_fraction = 0.02
 ecliptic_fit_apple = EllipticEnvelope(contamination=outliers_fraction).fit(pivot_Train)
 ecliptic_fit_apple.predict(pivot_Test)
