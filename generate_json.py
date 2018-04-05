6f0c244f453539373c1bd233bc48ef718eb8e02d
koatliky <koatliky@gmail.com>
2018-04-04 23:12:11 -0400

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -9,6 +9,7 @@ def app_data (icon, title, country, rank, chart, developer, url):
     data = '{' +'\n' + '"title": "' + title + '", "text": "Highest Rank Country: ' + country + '\t Current Rank: ' + rank + '\t Chart: ' + chart + '\t Developer: ' + developer + '", "thumb_url": "' + url +  '"},'
     return data
 
+#TODO once the input to python is decided, we will need a function that will loop through apps like code below:
 datam = ''
 for i in range (0,3):
     datam = datam+app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')
