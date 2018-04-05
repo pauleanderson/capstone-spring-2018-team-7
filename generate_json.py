1dec3170da7a51acb056599486de9f1185cfa325
koatliky <koatliky@gmail.com>
2018-04-04 23:09:49 -0400

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -13,19 +13,7 @@ datam = ''
 for i in range (0,3):
     datam = datam+app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')
 
-#print(generate_data(datam))
-
-def gen_app_data(icon, title, country, rank, chart, developer, url):
-    return json.dumps({'title': title, 'text': 'Highest Rank Country: ' + country + '\t Current Rank: ' + rank + '\t Chart: ' + chart + '\n Developer: ' + developer, 'thumb_url': url}, separators=(',', ':'))
-
-def gen_message (obj):
-    return json.dumps({'text': "*Cayce heils you!* \n The update for today:", 'attachments': [obj]})
-
-
-#with open('data.json', 'w') as outfile:
- #   json.dump(generate_data(app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'url')), outfile)
-
-file = open('data.json', 'w')
+file = open('data2.json', 'w')
 #file.write(generate_data(app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')))
 file.write(generate_data(datam))
 file.close()
