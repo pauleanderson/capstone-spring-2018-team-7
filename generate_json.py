2794bdac9b22ada37df7e1521b916b01bb4f0a64
koatliky <koatliky@gmail.com>
2018-04-12 14:51:56 -0400

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -64,20 +64,25 @@ print(apple_icon)
 print(android_icon)
 
 def generate_data (string):
-    string = '{' +'\n' + '"text":"*Cayce heils you!* The update for today:", "attachments": [' + string[:-1]
+    string = '{' +'\n' + '"text":"*Good morning from Cayce!* The update for today:", "attachments": [' + string[:-1]
     string = string + ']}'
     return string
 
-def app_data (icon, title, country, rank, chart, developer, url):
-    data = '{' +'\n' + '"title": "' + title + '", "text": "Highest Rank Country: ' + country + '\t Current Rank: ' + rank + '\t Chart: ' + chart + '\t Developer: ' + developer + '", "thumb_url": "' + url +  '"},'
+def app_data (platform, icon, title, countries, developer, url, release=None):
+    if (release == "Android"):
+        data = '{' +'\n' + '"title": "' + title + '", "text": "Highest Rank Country: ' + countries + '\t Developer: ' + developer + '", "thumb_url": "' + url +  '"},'
+    else:
+        data = '{' +'\n' + '"title": "' + title + '", "text": "Highest Rank Country: ' + countries + '\t Release Date: ' + release + '\t Developer: ' + developer + '", "thumb_url": "' + url +  '"},'        
     return data
 
 #TODO once the input to python is decided, we will need a function that will loop through apps like code below:
 datam = ''
 for i in range (0,3):
-    datam = datam+app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')
+    datam = datam+app_data('Apple', 'icon', 'Dragon Times', 'Country: au, rank: 100', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')
 
 file = open('data2.json', 'w')
-#file.write(generate_data(app_data('icon', 'Dragon Times', 'AU', '100', 'Free', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340')))
+
+file.write(generate_data(app_data('Apple', 'icon', 'Dragon Times', 'Country: au, rank: 100', 'crazy_developer', 'https://lh3.googleusercontent.com/lZhL5smuoS8EqylXKUgv5tN7V9jlSUT2txzyHYHcIBDwTLkKjDGmuEXDS5N6YB5Oag=w340', release = "10/20")))
 file.write(generate_data(datam))
 file.close()
+
