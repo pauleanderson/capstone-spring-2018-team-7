6d66b5b48e39dfb509c6a4890af18f7a29e9fbe4
katiebalcewicz <kh.balcewicz@gmail.com>
2018-04-12 15:26:01 -0400

    fix bash script bot

--- bot.sh
+++ bot.sh
@@ -1,2 +1,1 @@
-$ curl -X POST -H "Content-Type: application/json" \  --data @final.json https://hooks.slack.com/services/T91
-7Z9TPW/BA22BTNBY/FhWKmmFAYDGi0UcfhtSMFZf8
+curl -X POST -H "Content-Type: application/json" \  --data @data2.json https://hooks.slack.com/services/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
