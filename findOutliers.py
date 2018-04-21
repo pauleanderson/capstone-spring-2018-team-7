d13806892aaab5a8b86a707c36287d9bdc26ec54
katiebalcewicz <kh.balcewicz@gmail.com>
2018-04-21 19:20:40 -0400

    removing debugging statements

--- findOutliers.py
+++ findOutliers.py
@@ -180,13 +180,13 @@ if(len(df_android_outliers) > 0):
   androidOutlier = df_android_outliers.index[0]
 
 ###################
-print(outliers_apple)
-print(df_apple_outliers['max'])
-print(appleOutlier)
-print()
-print(outliers_android)
-print(df_android_outliers['max'])
-print(androidOutlier)
+# print(outliers_apple)
+# print(df_apple_outliers['max'])
+# print(appleOutlier)
+# print()
+# print(outliers_android)
+# print(df_android_outliers['max'])
+# print(androidOutlier)
 ####################
 
 
