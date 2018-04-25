1fe22e3d22404cb498ad6467d0103f891c90e9e3
katiebalcewicz <kh.balcewicz@gmail.com>
2018-04-25 05:21:01 -0400

    adding change filepath to bot

--- bot.sh
+++ bot.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd ~/capstone-spring-2018-team-7
 curl -X POST -H "Content-Type: application/json" \  --data @slack.json https://hooks.slack.com/services/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
--- cayce.sh
+++ cayce.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd ~/capstone-spring-2018-team-7
 curl -X POST -H "Content-Type: application/json" \  --data @slack.json https://hooks.slack.com/services/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
